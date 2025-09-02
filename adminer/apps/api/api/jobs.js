392932dd7d5ea10e4d43e9834684f95489d5a36c
Damien <engagehubonline@gmail.com>
2025-09-03 09:32:19 +1000

    fix: Revert to ES module format for Vercel compatibility
    - Use export default like other working endpoints
    - Remove CommonJS module.exports
    - Should work with Vercel serverless functions

--- adminer/apps/api/api/jobs.js
+++ adminer/apps/api/api/jobs.js
@@ -13,7 +13,7 @@ async function checkQuota(orgId, amount) {
   return true;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
